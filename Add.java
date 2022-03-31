cf88a713bc20718a303b8099fbf94e653d247842
VijayaEndluri <endluri.vijayasanthi@gmail.com>
2022-03-31 22:34:23 +0530

    Update Changes

--- Add.java
+++ Add.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");
      System.out.println  ("hi");
+       System.out.println  ("Address");
     }  
 }  
